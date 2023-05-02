6725ff64212ff5190f7e449b77a7b603c366ca42
billettc <charles@cbillette.com>
2023-05-02 13:47:06 -0400

    Ready to load!

---
+++ cmd/gma_loader/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"github.com/daedaleanai/ublox"
+	"github.com/daedaleanai/ublox/ubx"
+	"github.com/tarm/serial"
+)
+
+func main() {
+
+	config := &serial.Config{
+		Name:     "/dev/ttyAMA1", //todo: make this configurable
+		Baud:     38400,
+		Parity:   serial.ParityNone,
+		StopBits: serial.Stop1,
+	}
+
+	mgaOfflineFilePath := os.Args[1]
+	if _, err := os.Stat(mgaOfflineFilePath); errors.Is(err, os.ErrNotExist) {
+		fmt.Printf("File %s does not exist\n", mgaOfflineFilePath)
+		os.Exit(1)
+	}
+
+	mgaOfflineFile, err := os.Open(mgaOfflineFilePath)
+	handleError("opening mga file", err)
+
+	stream, err := serial.OpenPort(config)
+	handleError("opening gps serial port", err)
+
+	messageChannel := make(chan interface{})
+	go func() {
+		d := ublox.NewDecoder(stream)
+		for {
+			msg, err := d.Decode()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				handleError("decoding ubx", err)
+			}
+			messageChannel <- msg
+		}
+	}()
+
+	start := time.Now()
+	mgaOfflineDecoder := ublox.NewDecoder(mgaOfflineFile)
+	for {
+		msg, err := mgaOfflineDecoder.Decode()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			handleError("decoding ubx from mga offline file", err)
+		}
+		ano := msg.(*ubx.MgaAno)
+		anoDate := time.Date(int(ano.Year)+2000, time.Month(ano.Month), int(ano.Day), 0, 0, 0, 0, time.UTC)
+
+		if anoDate.Year() == 2023 && anoDate.Month() == 5 && anoDate.Day() == 2 { //todo: get system date
+			encoded, err := ubx.Encode(msg.(ubx.Message))
+			handleError("encoding ubx", err)
+			_, err = stream.Write(encoded)
+			handleError("writing to gpsd", err)
+			fmt.Printf("Sent: %#v\n", msg)
+
+			var ack *ubx.MgaAckData0
+			for {
+				fmt.Println("Waiting for ack")
+				select {
+				case msg := <-messageChannel:
+					if a, ok := msg.(*ubx.MgaAckData0); ok {
+						ack = a
+						fmt.Println("Got ack:", ack)
+					}
+				case <-time.After(1 * time.Second):
+					panic("Timed out")
+				}
+				if ack != nil {
+					break
+				}
+			}
+		}
+	}
+	fmt.Println("Send all ubx.MgaAno messageChannel", time.Since(start))
+}
+
+func handleError(context string, err error) {
+	if err != nil {
+		log.Fatalln(fmt.Sprintf("%s: %s\n", context, err.Error()))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module gnss_assistnow_offline
+
+go 1.20
+
+require (
+	github.com/daedaleanai/ublox v0.0.0-20210116232802-16609b0f9f43
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+)
+
+require golang.org/x/sys v0.7.0 // indirect
