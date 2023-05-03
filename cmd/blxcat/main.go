a5b21f995918b0bb869edbbd74ea8218fbb9d55b
billettc <charles@cbillette.com>
2023-05-03 13:43:57 -0400

    some test code

--- cmd/blxcat/main.go
+++ cmd/blxcat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/daedaleanai/ublox"
 	"github.com/daedaleanai/ublox/ubx"
@@ -33,6 +34,8 @@ func main() {
 				}
 				handleError("decoding ubx", err)
 			}
+
+			fmt.Printf("adding message to chan %T\n", msg)
 			messageChannel <- msg
 		}
 	}()
@@ -44,11 +47,13 @@ func main() {
 			fmt.Println("NavPvt info, date validity:", m.Valid, "accuracy:", m.TAcc_ns, "lock type:", m.FixType, "flags:", m.Flags, "flags2:", m.Flags2, "flags3:", m.Flags3)
 			d := time.Date(int(m.Year_y), time.Month(int(m.Month_month)), int(m.Day_d), int(m.Hour_h), int(m.Min_min), int(m.Sec_s), int(m.Nano_ns), time.UTC)
 			fmt.Println("Time:", d)
+		case *ubx.RawMessage:
+			class := []byte{byte(m.ClassID), byte(m.ClassID >> 8)}
+			fmt.Println("RawMessage hex:", hex.EncodeToString(class), hex.EncodeToString(m.Data))
 		default:
 			fmt.Printf("Received unknown message type %T\n", msg)
 		}
 	}
-
 }
 
 func handleError(context string, err error) {
